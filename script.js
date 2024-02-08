4854550fefec664f53a6f7753707ba37a127d8bd
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2024-02-08 10:15:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-// Initialize count and streak from localStorage or default to 0 and "Streak: No streak so far"
-let count = parseInt(localStorage.getItem('count')) || 0;
-let streak = localStorage.getItem('streak') || "No streak so far";
+let count = 0;
+let streakO = "Streak: No streak so far";
 
-// Update the HTML elements with initial count and streak values
-document.getElementById("daysOnTrack").innerText = count;
-document.getElementById("streak").textContent = streak;
-
-// Function to increment count and update streak
 function increment() {
     count++;
-    localStorage.setItem('count', count);
-    document.getElementById("daysOnTrack").innerText = count;
+    document.getElementById("daysOnTrack").innerText = count; 
+      
 }
 
-// Function to reset count and update streak
 function startOver() {
-    streak += count + " - ";
-    localStorage.setItem('streak', streak);
-    localStorage.setItem('count', 0);
-    start = start;
+    document.getElementById("daysOnTrack").innerText = count;    
+    document.getElementById("streak").textContent += count + " - "
     count = 0;
-    document.getElementById("daysOnTrack").innerText = count;
-    document.getElementById("streak").textContent = streak;
-}
